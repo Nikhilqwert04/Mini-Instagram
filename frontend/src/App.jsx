1563fe5d68f4aead1e1d754f3e8929db786924b2
Nikhilqwert04 <nikhilluthra04052007@gmail.com>
2026-08-02 11:35:10 +0530

    style: standardize quote usage and formatting in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Signup from './components/authentication/signup'
-import Signin from './components/authentication/signin'
-import Admin from './components/authentication/admin'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./components/authentication/signup";
+import Signin from "./components/authentication/signin";
+import Admin from "./components/authentication/admin";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Signup />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/admin' element={<Admin />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
