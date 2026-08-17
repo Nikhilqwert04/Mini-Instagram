628b20dfd506a76802a83a9c4b39961b0ac4aa45
Nikhilqwert04 <nikhilluthra04052007@gmail.com>
2026-08-17 10:49:32 +0530

    chore: update socket connection URL to local development server

--- frontend/src/components/Dashboard/pages/chat.jsx
+++ frontend/src/components/Dashboard/pages/chat.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://mini-instagram-1-yy2m.onrender.com" );
+const socket = io("https://localhost:3000" );
 
 const Chat = () => {
   const [message, setMessage] = useState("");
